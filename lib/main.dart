182677e4e077689c57a3bdec1d60d6d4f506f6d8
Bhumika Kalaru <bhumikakalaru@gmail.com>
2023-03-07 20:10:55 +0530

    app running after million years

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:alie/pages/*';
+import 'package:alie/pages/homePage.dart';
 // import 'package:alie/googlesignin.dart';
 // import 'package:alie/pages/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
+import 'googlesignin.dart';
 import 'pages/loginpage.dart';
 
 Future main() async {
